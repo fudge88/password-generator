538c51cdfe40a97d7b4157c53b8972f4e8710ef3
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-09-27 13:44:47 +0100

    restarted homework- boiler plate for functiosn added

--- script.js
+++ script.js
@@ -9,63 +9,25 @@
 //   passwordText.value = password;
 // }
 
-// declare validation function
-function validate(pwLength) {
-  if (pwLength >= 0 && typeof pwLength === "number") {
-    return true;
-  } else {
-    return false;
-  }
+// get criteria
+const getCriteria () {
+return 
 }
 
-// get criteria
-// length of password
-const pwLength = 3;
-
-if (validate(pwLength)) {
-  if (pwLength >= 8 && pwLength <= 128) {
-    console.log("correct length input");
-  } else {
-    console.log("Incorrect length input");
-  }
+// validate
+const validateCriteria () {
+return
 }
 
 // looping function
+const generatePassword () {
+return
+}
 
-// generate random password
-
-// criteria
-
-// const lowerCase = [
-//   "a",
-//   "b",
-//   "c",
-//   "d",
-//   "e",
-//   "f",
-//   "g",
-//   "h",
-//   "i",
-//   "j",
-//   "k",
-//   "l",
-//   "m",
-//   "n",
-//   "o",
-//   "p",
-//   "q",
-//   "r",
-//   "s",
-//   "t",
-//   "u",
-//   "v",
-//   "w",
-//   "x",
-//   "y",
-//   "z",
-// ];
-// const specialCharacter = ["!", "£", "$", "&", "*", ".", "?", "@", "#", "/"];
-// const digit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// generate password
+const displayPassword () {
+return
+}
 
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
