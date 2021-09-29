879f6f604fd7c36246582691707c2cc8732acb41
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-09-29 10:58:16 +0100

    arrays added

--- script.js
+++ script.js
@@ -7,7 +7,71 @@
 //   var passwordText = document.querySelector("#password");
 
 //   passwordText.value = password;
-// }
+
+// Arrays
+const num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const special = [
+  "!",
+  "£",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "?",
+  "@",
+  "#",
+  "~",
+  "|",
+  "/",
+  ".",
+  ",",
+  "<",
+  ">",
+  "'",
+  ":",
+  " ",
+  ";",
+  "`",
+  "¬",
+];
+const lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const upperCase = lowerCase.join("").toUpperCase();
+const upperCase = upperCase.split("");
 
 // get criteria
 // get password length ***works****
