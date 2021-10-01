a0931ae7bc8f7803d00d9ff2da5b81988fb8c403
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-10-01 23:00:12 +0100

    bug fix- uppercse added to generatepw fucntion"

--- script.js
+++ script.js
@@ -1,77 +1,71 @@
 // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
+var generateBtn = document.querySelector("#generate");
 
 // Arrays
-const numArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const specialArray = [
-  '!',
-  '£',
-  '$',
-  '%',
-  '^',
-  '&',
-  '*',
-  '(',
-  ')',
-  '_',
-  '-',
-  '+',
-  '=',
-  '?',
-  '@',
-  '#',
-  '~',
-  '|',
-  '/',
-  '.',
-  ',',
-  '<',
-  '>',
+  "!",
+  "£",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "?",
+  "@",
+  "#",
+  "~",
+  "|",
+  "/",
+  ".",
+  ",",
+  "<",
+  ">",
   '"',
-  ':',
-  ' ',
-  ';',
-  '`',
-  '¬',
+  ":",
+  " ",
+  ";",
+  "`",
+  "¬",
 ];
 const lowerCaseArray = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z',
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
-const upperCaseArray = lowerCaseArray.join("").toUpperCase();
-const upperCaseArray = upperCaseArray.split("");
+// convert lower case to uppercase array
+const upperCase = lowerCaseArray.join("").toUpperCase();
+const upperCaseArray = upperCase.split("");
 
 // get criteria
 // Function - prompt user to select password characteristics
@@ -80,67 +74,60 @@ const getCriteria = function () {
   const x = prompt("Choose Password length");
   // variable is converted to a number and passed into a new variable called passwordLength
   const passwordLength = parseInt(x, 10);
-  // checks if the value passed is a number, 
+  // checks if the value passed is a number,
   if (Number.isNaN(passwordLength)) {
     // if it is not a number the return the following message
-    alert ("Please enter a valid number");
+    alert("Please enter a valid number");
     // then break away
- return;
-}
-
-// length requirement error handler
-  if (passwordLength < 8  || passwordLength > 128) {
-    // if number is of an incorrect length the return the following message
-    alert ("Please enter a number between 8-128");
-    // then break away
- return;
-}
-
-// variable assigning each criteria the user can choose from
-// Confirmation box - pop up asking user for boolean data type in return 
-const lowerCase = confirm("Password to contain lowercase characters?");
-const upperCase = confirm("Password to contain uppercase characters?");
-const specialChar = confirm("Password to contain special characters?");
-const numChar = confirm("Password to contain numbers?");
-// created an object called 'attributes', holding all the variables the user will return
-const attributes = {
-  passwordLength: passwordLength,
-  lowerCase: lowerCase,
-  upperCase: upperCase,
-  specialChar: specialChar,
-  numChar: numChar,
-};
-return attributes;
-};
-
-const getUpperCase = function () {
-  const upperCase = confirm("Would you like to use uppercase characters?");
-  if (upperCase === true) {
-    console.log("YES");
-  } else {
-    console.log("NO");
+    return;
   }
+
+  // length requirement error handler
+  if (passwordLength < 8 || passwordLength > 128) {
+    // if number is of an incorrect length the return the following message
+    alert("Please enter a number between 8-128");
+    // then break away
+    return;
+  }
+
+  // variable assigning each criteria the user can choose from
+  // Confirmation box - pop up asking user for boolean data type in return
+  const lowerCase = confirm("Password to contain lowercase characters?");
+  const upperCase = confirm("Password to contain uppercase characters?");
+  const specialChar = confirm("Password to contain special characters?");
+  const numChar = confirm("Password to contain numbers?");
+  // created an object called 'attributes', holding all the variables the user will return
+  const attributes = {
+    passwordLength: passwordLength,
+    lowerCase: lowerCase,
+    upperCase: upperCase,
+    specialChar: specialChar,
+    numChar: numChar,
+  };
+  return attributes;
 };
 
 // function that generates the password according to user choices
 const generatePassword = function () {
+  // call on getCriteria function to use variables
+  const attributes = getCriteria();
   // empty array that will hold the arrays the user wishes to use
   const containerArray = [];
   // if user has received error message finishes the function
   if (!attributes) {
     return;
   }
-// if the named attributes are selected, push into the empty array using the spread function
-// container array goes first as thats the container we want to fill, push is the function to add,
-// the ...is the spread function that we attach to the array we are forcing into a new array.
+  // if the named attributes are selected, push into the empty array using the spread function
+  // container array goes first as thats the container we want to fill, push is the function to add,
+  // the ...is the spread function that we attach to the array we are forcing into a new array.
 
-// if in the object attributes the variable of upperCase is present add the lowercase array to the empty array
+  // if in the object attributes the variable of upperCase is present add the lowercase array to the empty array
   if (attributes.upperCase === true) {
-    containerArray.push(...lowerCaseArray);
-  } 
+    containerArray.push(...upperCaseArray);
+  }
   if (attributes.lowerCase === true) {
     containerArray.push(...lowerCaseArray);
-  } 
+  }
   if (attributes.numChar === true) {
     containerArray.push(...numArray);
   }
@@ -150,44 +137,37 @@ const generatePassword = function () {
   // if the user has NOT picked any attributes display error message
   if (!containerArray.length) {
     alert("You MUST choose at least one type of character");
+    // break off
     return;
   }
-// // do you want numeric characters
-// function num() {}
-// // do you want special characters
-// function special() {}
 
-// // looping function
-var i = 0;
-for (i = 0; i < pwLength.length; i++) {
-  // get a random character from uppercase array
-  var upperCaseCharacter = upperCaseArray.charAt(
-    Math.floor(Math.random() * upperCaseArray.length)
-  );
-  // get a random character from lowercase array
-  var lowerCaseCharacter = lowerCaseArray.charAt(
-    Math.floor(Math.random() * lowerCaseArray.length)
-  );
-  // get a random character from special character array
-  var specialCharacter = specialArray.charAt(
-    Math.floor(Math.random() * specialArray.length)
-  );
-  // get a random character from numeric array
-  var numberCharacter = numArray.charAt(
-    Math.floor(Math.random() * numArray.length)
-  );
+  //  empty array that will holder users random password
+  const passwordArray = [];
+
+  // for loop
+  // index to start at 0, targeting the attributes object and the passwordLength variable to get number of characters
+  // and for the function to be incremented until the password length is met
+  for (let i = 0; i < attributes.passwordLength; i++) {
+    // variable called randomIndex, (rounding down) random whole number found in the length of the
+    // containerArray which contains all the users chosen attributes
+    const randomIndex = Math.floor(Math.random() * containerArray.length);
+    // variable called randomChar  takes on the value on the randomIndex from tehe containerArray
+    const randomChar = containerArray[randomIndex];
+    // pushing the randomChar into the passwordArray
+    passwordArray.push(randomChar);
+  }
+  // this turns the array into a string
+  const password = passwordArray.join("");
+  // creates password
+  return password;
+};
+
+// // Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
-// };
-
-// // generate password
-// const displayPassword = function () {
-//   return;
-// };
-
-// // Add event listener to generate button
-// // generateBtn.addEventListener("click", writePassword);
-
-// // get user criteria
-
-// const pwLength = getCriteria();
-// console.log(pwLength);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
