e8d8b29506db45d2cb99e9a4d8b62fbf8717e5c6
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-10-01 22:10:09 +0100

    changed the getCriteria function, by changing the use of the number function

--- script.js
+++ script.js
@@ -9,81 +9,85 @@
 //   passwordText.value = password;
 
 // Arrays
-const numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const numArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 const specialArray = [
-  "!",
-  "£",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "?",
-  "@",
-  "#",
-  "~",
-  "|",
-  "/",
-  ".",
-  ",",
-  "<",
-  ">",
-  "'",
-  ":",
-  " ",
-  ";",
-  "`",
-  "¬",
+  '!',
+  '£',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '?',
+  '@',
+  '#',
+  '~',
+  '|',
+  '/',
+  '.',
+  ',',
+  '<',
+  '>',
+  '"',
+  ':',
+  ' ',
+  ';',
+  '`',
+  '¬',
 ];
 const lowerCaseArray = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
 ];
 
-// const upperCaseArray = lowerCaseArray.join("").toUpperCase();
-// const upperCaseArray = upperCaseArray.split("");
+const upperCaseArray = lowerCaseArray.join("").toUpperCase();
+const upperCaseArray = upperCaseArray.split("");
 
-// // get criteria
-// // get password length ***works****
-// const getCriteria = function () {
-//   const pwLength = Number(prompt("Choose Password length"));
-//   // eliminate the possibility of a letter instead of a number
-//   if (isNaN(pwLength)) {
-//     return "Not a Valid number!";
-//   } else {
-//     return "you have chosen " + pwLength;
-//   }
-// };
+// get criteria
+// Function - prompt user to select password characteristics
+const getCriteria = function () {
+  // created a variable x which will hold the users numerical input
+  const x = prompt("Choose Password length");
+  // variable is converted to a number and passed into a new variable called passwordLength
+  const passwordLength = parseInt(x, 10);
+  // checks if the value passed is a number, 
+  if (number.isNaN(passwordLength)) {
+    // if it is not a number the return the following message
+    return "Please enter a valid number";
+  } else {
+    return "you have chosen " + pwLength;
+  }
+};
 
 // length requirement function works
 // function validate(pwLength) {
@@ -111,11 +115,23 @@ var num = "";
 var special = "";
 
 // // do you want uppercase characters
-prompt("Do you want an Uppercase character");
-if (upperCase === true) {
-  console.log("you want an uppercase character");
-} else {
-  console.log("you NO NOT want an uppercase character");
+
+
+const getUpperCase = function () {
+  const upperCase = confirm("Would you like to use uppercase characters?");
+  if (upperCase === true) {
+    console.log("YES");
+  } else {
+    console.log("NO");
+  }
+};
+
+const getUpperCase = function () {
+  const = prompt("Do you want an Uppercase character");
+  if (upperCase === true) {
+    console.log("you want an uppercase character");
+  } else {
+    console.log("you NO NOT want an uppercase character");
 }
 
 prompt("Do you want an Lowercase character");
@@ -144,7 +160,25 @@ if (special === true) {
 // function special() {}
 
 // // looping function
-
+var i = 0;
+for (i = 0; i < pwLength.length; i++) {
+  // get a random character from uppercase array
+  var upperCaseCharacter = upperCaseArray.charAt(
+    Math.floor(Math.random() * upperCaseArray.length)
+  );
+  // get a random character from lowercase array
+  var lowerCaseCharacter = lowerCaseArray.charAt(
+    Math.floor(Math.random() * lowerCaseArray.length)
+  );
+  // get a random character from special character array
+  var specialCharacter = specialArray.charAt(
+    Math.floor(Math.random() * specialArray.length)
+  );
+  // get a random character from numeric array
+  var numberCharacter = numArray.charAt(
+    Math.floor(Math.random() * numArray.length)
+  );
+}
 // };
 
 // // generate password
